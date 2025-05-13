e86e3f382f97aa17941733a0198f0cce62398c4d
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-13 14:55:43 +0700

    Verify user unable to login with locked out user

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,10 +9,20 @@ test('has title', async ({ page }) => {
 
 test('Generating tests', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="login-credentials"]').click();
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
 });
+
+test('Verify user unable to login with locked out user', async({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('locked_out_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  const errorMessage = await page.locator('[data-test="error"]').textContent();
+  expect(errorMessage).toContain('Epic sadface: Sorry, this user has been locked out.'); 
+})
