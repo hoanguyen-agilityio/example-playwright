7e87280b7d332337e387864e0bb271fd7b99a301
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-13 15:41:11 +0700

    Verify user is able to navigate to 'All Items' page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -34,8 +34,21 @@ test('Verify user is able to navigate to "Your cart" page', async ({ page }) => 
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
-  await page.goto('https://www.saucedemo.com/inventory.html');
   await page.locator('[data-test="shopping-cart-link"]').click();
   const cartTitle = await page.locator('.title').textContent();
   expect(cartTitle).toContain('Your Cart');
+});
+
+test('Verify user is able to navigate to "All Items" page', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('.bm-burger-button').click();
+  await page.locator('[data-test="inventory-sidebar-link"]').click();
+  const allItemsTitle = await page.locator('.title').textContent();
+  expect(allItemsTitle).toContain('Products');
 })
