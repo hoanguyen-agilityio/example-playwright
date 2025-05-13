73ebcf481825f42d9735b592f8df767782483912
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-13 15:25:28 +0700

    Verify user is able to navigate to 'Your cart' page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,7 +16,7 @@ test('Generating tests', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click();
 });
 
-test('Verify user unable to login with locked out user', async({ page }) => {
+test('Verify user unable to login with locked out user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('locked_out_user');
@@ -24,5 +24,18 @@ test('Verify user unable to login with locked out user', async({ page }) => {
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
   const errorMessage = await page.locator('[data-test="error"]').textContent();
-  expect(errorMessage).toContain('Epic sadface: Sorry, this user has been locked out.'); 
+  expect(errorMessage).toContain('Epic sadface: Sorry, this user has been locked out.');
+});
+
+test('Verify user is able to navigate to "Your cart" page', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.goto('https://www.saucedemo.com/inventory.html');
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  const cartTitle = await page.locator('.title').textContent();
+  expect(cartTitle).toContain('Your Cart');
 })
