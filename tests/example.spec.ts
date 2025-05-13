4eb0c6e593affc9a1ecf5b58d42d547a90e8d147
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-13 15:53:05 +0700

    Verify user is able to add item to cart and checkout successful

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -51,4 +51,26 @@ test('Verify user is able to navigate to "All Items" page', async ({ page }) => 
   await page.locator('[data-test="inventory-sidebar-link"]').click();
   const allItemsTitle = await page.locator('.title').textContent();
   expect(allItemsTitle).toContain('Products');
-})
+});
+
+test('Verify user is able to add item to cart and checkout successful', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Hoa');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Nguyen');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('12345');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  const checkoutSuccessfulTitle = await page.locator('.title').textContent();
+  expect(checkoutSuccessfulTitle).toContain('Checkout: Complete!');
+});
