509be833dca901a8d27f598d4b3f921b3c9f2947
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-14 10:02:13 +0700

    Create variable for locator

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,86 +9,124 @@ test('Verify title contains "Swag Labs"', async ({ page }) => {
 
 test('Verify user is able to login successful with standard user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  const inventoryTitle = await page.locator('.title').textContent();
+
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
+  const title = page.locator('.title');
+
+  await usernameInput.fill('standard_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
+
+  const inventoryTitle = await title.textContent();
   expect(inventoryTitle).toContain('Products');
 });
 
 test('Verify user gets error when logging in as locked out user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('locked_out_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  const errorMessage = await page.locator('[data-test="error"]').textContent();
+
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
+  const errorMessageLocator = page.locator('[data-test="error"]');
+
+  await usernameInput.fill('locked_out_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
+
+  const errorMessage = await errorMessageLocator.textContent();
   expect(errorMessage).toContain('Epic sadface: Sorry, this user has been locked out.');
 });
 
 test('Verify user is able to access cart page', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  const cartTitle = await page.locator('.title').textContent();
+
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
+  const cartButton = page.locator('[data-test="shopping-cart-link"]');
+  const cartTitleLocator = page.locator('.title');
+
+  await usernameInput.fill('standard_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
+  await cartButton.click();
+
+  const cartTitle = await cartTitleLocator.textContent();
   expect(cartTitle).toContain('Your Cart');
 });
 
 test('Verify user can navigate from cart to all items', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  await page.locator('.bm-burger-button').click();
-  await page.locator('[data-test="inventory-sidebar-link"]').click();
-  const allItemsTitle = await page.locator('.title').textContent();
+
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
+  const cartButton = page.locator('[data-test="shopping-cart-link"]');
+  const burgerMenuButton = page.locator('.bm-burger-button');
+  const allItemButton = page.locator('[data-test="inventory-sidebar-link"]');
+  const allItemsTitleLocator = page.locator('.title');
+
+  
+  await usernameInput.fill('standard_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
+  await cartButton.click();
+  await burgerMenuButton.click();
+  await allItemButton.click();
+
+  const allItemsTitle = await allItemsTitleLocator.textContent();
   expect(allItemsTitle).toContain('Products');
 });
 
 test('Verify checkout success after adding item', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('[data-test="shopping-cart-link"]').click();
-  await page.locator('[data-test="checkout"]').click();
-  await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('Hoa');
-  await page.locator('[data-test="lastName"]').click();
-  await page.locator('[data-test="lastName"]').fill('Nguyen');
-  await page.locator('[data-test="postalCode"]').click();
-  await page.locator('[data-test="postalCode"]').fill('12345');
-  await page.locator('[data-test="continue"]').click();
-  await page.locator('[data-test="finish"]').click();
-  const checkoutSuccessfulTitle = await page.locator('.title').textContent();
+
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
+  const addToCartButton = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
+  const cartButton = page.locator('[data-test="shopping-cart-link"]');
+  const checkoutButton = page.locator('[data-test="checkout"]');
+  const firstNameInput = page.locator('[data-test="firstName"]');
+  const lastNameInput = page.locator('[data-test="lastName"]');
+  const postalCodeInput = page.locator('[data-test="postalCode"]');
+  const continueButton = page.locator('[data-test="continue"]');
+  const finishButton = page.locator('[data-test="finish"]');
+  const checkoutSuccessfulTitleLocator = page.locator('.title');
+
+  await usernameInput.fill('standard_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
+  await addToCartButton.click();
+  await cartButton.click();
+  await checkoutButton.click();
+  await firstNameInput.fill('Hoa');
+  await lastNameInput.fill('Nguyen');
+  await postalCodeInput.fill('12345');
+  await continueButton.click();
+  await finishButton.click();
+
+  const checkoutSuccessfulTitle = await checkoutSuccessfulTitleLocator.textContent();
   expect(checkoutSuccessfulTitle).toContain('Checkout: Complete!');
 });
 
 test('Verify sorting of items by price ascending', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
 
+  const usernameInput = page.locator('[data-test="username"]');
+  const passwordInput = page.locator('[data-test="password"]');
+  const loginButton = page.locator('[data-test="login-button"]');
   const sortSelect = page.locator('[data-test="product-sort-container"]');
   const lohiOption = sortSelect.locator('option[value="lohi"]');
-
+  
+  await usernameInput.fill('standard_user');
+  await passwordInput.fill('secret_sauce');
+  await loginButton.click();
   await expect(lohiOption).toHaveText('Price (low to high)');
   await sortSelect.selectOption('lohi');
-
+  
   const firstItem = page.locator('[data-test="inventory-item"]').first();
   const priceText = await firstItem.locator('[data-test="inventory-item-price"]').textContent();
 
