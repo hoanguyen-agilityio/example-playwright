cbe5d8d05eb06d2ba70c1022d6583832b8afb7d5
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-16 14:44:13 +0700

    apply test.step()

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
+
+async function addItemToCart(page: Page, itemName: string) {
+  await test.step(`Add "${itemName}" to cart`, async () => {
+    const addToCartButton = page.locator('.inventory_item')
+      .filter({ hasText: itemName})
+      .getByRole('button', { name: 'Add to cart'})
+
+    await addToCartButton.click();
+  })
+}
+
+async function proceedToCheckout(cart: CartPage) {
+  await test.step('Proceed to checkout', async () => {
+    await cart.openCart();
+    await cart.checkoutButton.click();
+  });
+}
 
 test.describe('Login Tests', () => {
   test('Verify title contains "Swag Labs"', async ({ page }) => {
@@ -76,13 +93,8 @@ test.describe('Checkout Tests', () => {
     const cart = new CartPage(page);
     const checkout = new CheckoutPage(page);
 
-    const addToCartButton = page.locator('.inventory_item')
-      .filter({ hasText: 'Sauce Labs Backpack'})
-      .getByRole('button', { name: 'Add to cart'})
-  
-    await addToCartButton.click();
-    await cart.openCart();
-    await cart.checkoutButton.click();
+    await addItemToCart(page, 'Sauce Labs Backpack');
+    await proceedToCheckout(cart);
     await checkout.completeCheckout('Hoa', 'Nguyen', '12345');
     await expect(page).toHaveURL(/.*checkout-complete/);
   
@@ -106,7 +118,7 @@ test.describe('Sorting Tests', () => {
     await expect(page).toHaveURL(/.*inventory/);
     const prices = await inventory.getAllItemPrices();
     const sorted = [...prices].sort((a, b) => a - b);
-    
+
     expect(prices).toEqual(sorted);
   });
 })
