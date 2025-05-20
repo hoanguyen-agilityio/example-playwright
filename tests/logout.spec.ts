d5fce58f71b7de30a948b6e7f7c8e57563405628
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-20 15:50:38 +0700

    Create test case for successful logout

---
+++ tests/logout.spec.ts
@@ -0,0 +1,29 @@
+// Libs
+import test, { expect } from "@playwright/test";
+
+// Constants
+import { BASE_URL, PRODUCTS_URL } from "@/constants";
+
+test.describe('Logout Tests', () => {
+  test('Verify user is able to logout successfully', async ({ page }) => {
+    const menuButton = await page.getByRole('button', { name: 'Open Menu' });
+    const logoutLink = await page.getByTestId('logout-sidebar-link');
+
+    await test.step('Navigate to product page', async () => {
+      await page.goto(PRODUCTS_URL)
+    });
+
+    await test.step('Open menu', async () => {
+      await menuButton.click();
+    });
+
+    await test.step('Click on logout link', async () => {
+      await logoutLink.click();
+    })
+
+    await test.step('Verify user is logged out', async () => {
+      await expect(page).toHaveURL(BASE_URL);
+      await expect(page).toHaveTitle(/Swag Labs/);
+    });
+  })
+})
