f18f42d118ca81e3c886525f20a4a06dbbfcecc5
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-22 17:39:32 +0700

    Write test case for navigating to empty cart

--- tests/navigation-detail.spec.ts
+++ tests/navigation-detail.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@/fixtures";
-import { DETAILS_PAGE_URL, PRODUCTS, PRODUCTS_URL } from "@/constants";
+import { CART_URL, DETAILS_PAGE_URL, HEADINGS, PRODUCTS, PRODUCTS_URL } from "@/constants";
 
 test.describe('Navigation Detail Page', () => {
   test.beforeEach(async ({ page }) => {
@@ -28,5 +28,21 @@ test.describe('Navigation Detail Page', () => {
       await expect(page).toHaveURL(DETAILS_PAGE_URL);
       await expect(detailPage.productImage).toBeVisible();
     })
+  });
+
+  test('Verify user can access empty cart page', async ({ page, inventoryPage, cartPage }) => {
+    await test.step('Click on product title by name', async () => {
+      await inventoryPage.goToProductDetailByName(PRODUCTS.BACKPACK);
+    });
+
+    await test.step('Open cart', async () => {
+      await cartPage.openCart();
+    });
+
+    await test.step('Verify cart page is displayed', async () => {
+      await expect(page).toHaveURL(CART_URL);
+      await expect(cartPage.title).toBeVisible();
+      await expect(cartPage.title).toHaveText(HEADINGS.YOUR_CART);
+    });
   })
 });
