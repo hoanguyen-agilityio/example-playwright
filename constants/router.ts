1e09017e2c1f30669be5d130d784c79db8f6f284
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-05-22 14:27:17 +0700

    Write test case for user not being able to checkout with empty fields

--- constants/router.ts
+++ constants/router.ts
@@ -3,5 +3,6 @@ const PRODUCTS_URL = 'https://www.saucedemo.com/inventory.html';
 const CHECKOUT_URL = /.*checkout-complete/;
 const INVENTORY_URL = /.*inventory/;
 const CART_URL = /.*cart/;
+const CHECKOUT_STEP_ONE = /.*checkout-step-one/;
 
-export { BASE_URL, PRODUCTS_URL, CHECKOUT_URL, INVENTORY_URL, CART_URL };
+export { BASE_URL, PRODUCTS_URL, CHECKOUT_URL, INVENTORY_URL, CART_URL, CHECKOUT_STEP_ONE };
--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -2,41 +2,67 @@
 import { test, expect } from '@playwright/test';
 
 // Constants
-import { CHECKOUT_URL, HEADINGS, PRODUCTS, PRODUCTS_URL, USER } from '@/constants';
+import {
+  CHECKOUT_STEP_ONE,
+  CHECKOUT_URL,
+  ERROR_MESSAGES,
+  HEADINGS,
+  PRODUCTS,
+  PRODUCTS_URL,
+  USER
+} from '@/constants';
 
 // Pages
 import { CartPage, CheckoutPage } from '@/pages';
 
 test.describe('Checkout Tests', () => {
-  test('Verify checkout success after adding item', async ({ page }) => {
-    const cart = new CartPage(page);
-    const checkout = new CheckoutPage(page);
+  let cart: CartPage;
+  let checkout: CheckoutPage;
+
+  test.beforeEach(async ({ page }) => {
+    cart = new CartPage(page);
+    checkout = new CheckoutPage(page);
 
     await test.step('Navigate to product page', async () => {
-      await page.goto(PRODUCTS_URL)
-    })
+      await page.goto(PRODUCTS_URL);
+    });
 
-    await test.step(`Add "Sauce Labs Backpack" to cart`, async () => {
+    await test.step('Add "Sauce Labs Backpack" to cart', async () => {
       const addToCartButton = page.locator('.inventory_item')
         .filter({ hasText: PRODUCTS.BACKPACK })
-        .getByRole('button', { name: 'Add to cart' })
+        .getByRole('button', { name: 'Add to cart' });
 
       await addToCartButton.click();
-    })
+    });
 
     await test.step('Proceed to checkout', async () => {
       await cart.openCart();
       await cart.checkoutButton.click();
     });
+  });
 
+  test('Verify checkout success after adding item', async ({ page }) => {
     await test.step('Complete the checkout form', async () => {
       await checkout.completeCheckout(USER.FIRST_NAME, USER.LAST_NAME, USER.POSTAL_CODE);
-    })
+    });
 
     await test.step('Verify checkout completion', async () => {
       await expect(page).toHaveURL(CHECKOUT_URL);
       await expect(checkout.title).toBeVisible();
-      await expect(checkout.title).toHaveText(HEADINGS.CHECKOUT_COMPLETE)
-    })
+      await expect(checkout.title).toHaveText(HEADINGS.CHECKOUT_COMPLETE);
+    });
   });
-})
+
+  test('Verify user cannot checkout with empty fields', async ({ page }) => {
+    await test.step('Try to submit empty form', async () => {
+      await checkout.fillCheckoutForm(USER.EMPTY, USER.EMPTY, USER.EMPTY);
+      await checkout.continueButton.click();
+    });
+
+    await test.step('Verify checkout failure', async () => {
+      await expect(page).toHaveURL(CHECKOUT_STEP_ONE);
+      await expect(checkout.errorMessage).toBeVisible();
+      await expect(checkout.errorMessage).toHaveText(ERROR_MESSAGES.FIRST_NAME_REQUIRED);
+    });
+  });
+});
